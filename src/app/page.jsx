6bdba1b79a1dc158a5c5957818f7fe9a3d190605
kirankumarkm08 <kirankumarkm1505@gmail.com>
2024-10-07 10:30:07 +0530

    demoModel is updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,22 +9,30 @@ import DemoSwap from "@/components/DemoSwap";
 const page = () => {
   const { address } = useAccount();
   const { disconnect } = useDisconnect();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  });
 
   return (
     <div className="flex flex-col h-screen w-full items-center justify-center gap-3">
-      <div>{isClient && address && `wallet address: ${address}`}</div>
-      <div>
-        {address && (
+      {isClient && (
+        <div className="">
+          <h1 className="">{address && `wallet address: ${address}`}</h1>
           <div>
-            <button
-              className="bg-black px-2 py-4 text-white rounded-md"
-              onClick={() => disconnect()}
-            >
-              disconnect
-            </button>
+            {address && (
+              <button
+                className="bg-black px-2 py-4 text-white rounded-md flex justify-center w-full"
+                onClick={() => disconnect()}
+              >
+                disconnect
+              </button>
+            )}
           </div>
-        )}
-      </div>
+        </div>
+      )}
+
       <DemoSwap />
     </div>
   );
